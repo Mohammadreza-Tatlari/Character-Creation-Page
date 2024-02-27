d5238770a95380b79483a09350dca99f7deac738
Mohammadreza-Tatlari <mohammadreza.tatlari8@gmail.com>
2024-02-27 23:47:05 +0330

    the selecting volumes are now functional and have range of 1 to 355 in state and also will be disabled when the number is reached to its limit point

--- components/SelectingVolume.tsx
+++ components/SelectingVolume.tsx
@@ -1,42 +1,66 @@
-'use client'
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-interface SelectingVolumeProps{
+interface SelectingVolumeProps {
   title: String;
-  value?: number
+  value?: number;
 }
-export default function SelectingVolume({title , value=344}:SelectingVolumeProps) {
-    
+export default function SelectingVolume({
+  title,
+}: SelectingVolumeProps) {
+  const [volumeValue, setVolumeValue] = useState(350);
+
+  function handleDecrease() {
+    if (volumeValue > 1) {
+      setVolumeValue(volumeValue - 1);
+    }
+  }
+
+  function handleIncrease() {
+    if (volumeValue < 355) {
+      setVolumeValue(volumeValue + 1);
+    }
+  }
+
   return (
     <div className="w-[440px] h-[62px] overflow-hidden shrink-0 flex flex-col items-start justify-start py-0 pr-0 pl-px box-border gap-[1px_0px]">
-        <div className="relative tracking-[0.05em] leading-[140%] uppercase">
-         {title}
-        </div>
-        <div className="self-stretch flex flex-row items-start justify-start gap-[0px_3px] max-w-full text-base font-gilroy-semibold">
-          <div className="rounded-tl-8xs rounded-tr-none rounded-br-none rounded-bl-8xs bg-gray-400 flex flex-row items-center justify-center p-2.5">
-            <div className="h-10 w-10 relative rounded-tl-8xs rounded-tr-none rounded-br-none rounded-bl-8xs bg-gray-400 hidden" />
-            <img
-              className="h-5 w-5 relative object-contain z-[1]"
-              loading="eager"
-              alt=""
-              src="/arrowright.svg"
-            />
-          </div>
-          <div className="flex-1 bg-gray-400 flex flex-row items-center justify-center py-[9px] pr-[22px] pl-5 box-border min-w-[27px] max-w-full">
-            <div className="h-10 w-[354px] relative bg-gray-400 hidden max-w-full" />
-            <div className="relative tracking-[0.05em] leading-[140%] uppercase z-[1]">
-              {value}
-            </div>
-          </div>
-          <div className="h-10 w-10 relative">
-            <div className="absolute top-[40px] left-[40px] rounded-tl-8xs rounded-tr-none rounded-br-none rounded-bl-8xs bg-gray-400 w-full h-full [transform:_rotate(180deg)] [transform-origin:0_0]" />
-            <img
-              className="absolute top-[10px] left-[10px] w-5 h-5 z-[1]"
-              alt=""
-              src="/arrowright-1.svg"
-            />
-          </div>
-        </div>
+      <div className="relative tracking-[0.05em] leading-[140%] uppercase">
+        {title}
       </div>
-  )
+      <div className="self-stretch flex flex-row items-start justify-start gap-[0px_3px] max-w-full text-base font-gilroy-semibold">
+        <button onClick={handleDecrease} disabled={volumeValue == 1}
+        className="disabled:opacity-25 cursor-pointer rounded-tl-8xs rounded-tr-none rounded-br-none rounded-bl-8xs bg-gray-400 hover:bg-lawngreen-200 active:bg-lawngreen-100/70 group flex flex-row items-center justify-center p-2.5 ">
+          <img
+            className="h-5 w-5 relative object-contain z-[1]"
+            loading="eager"
+            alt=""
+            src="/arrowright.svg"
+          />
+        </button>
+        <div className="flex-1 bg-gray-400 flex flex-row items-center justify-center py-[9px] pr-[22px] pl-5 box-border min-w-[27px] max-w-full">
+          <div className="h-10 w-[354px] relative bg-gray-400 hidden max-w-full" />
+          <div className="relative tracking-[0.05em] leading-[140%] uppercase z-[1]">
+            {volumeValue}
+          </div>
+        </div>
+        <button onClick={handleIncrease} disabled={volumeValue == 355}
+        className="disabled:opacity-25 cursor-pointer rounded-tr-8xs rounded-tl-none rounded-bl-none rounded-br-8xs bg-gray-400 hover:bg-lawngreen-200 active:bg-lawngreen-100/70 group flex flex-row items-center justify-center p-2.5 ">
+          <img
+            className="h-5 w-5 relative object-contain z-[1]"
+            loading="eager"
+            alt=""
+            src="/arrowright-1.svg"
+          />
+        </button>
+        {/* <div className="h-10 w-10 relative group">
+          <div className="cursor-pointer absolute top-[40px] left-[40px] rounded-tl-8xs rounded-tr-none rounded-br-none rounded-bl-8xs bg-gray-400 hover:bg-lawngreen-200 active:bg-lawngreen-100/70 w-full h-full [transform:_rotate(180deg)] [transform-origin:0_0] group" />
+          <img
+            className=" relative top-[10px] left-[10px] w-5 h-5 z-[1] cursor-pointer"
+            alt=""
+            src="/arrowright-1.svg"
+          />
+        </div> */}
+      </div>
+    </div>
+  );
 }
